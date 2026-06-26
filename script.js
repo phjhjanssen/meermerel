029a925857883d62b897363b9422e778b740d2dc
phjhjanssen <154504183+phjhjanssen@users.noreply.github.com>
2026-06-26 10:40:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -189,13 +189,13 @@ const ARTWORKS = [
   {
     id: 'w2601',
     year: 2026,
-    title: '',
-    medium: '',
-    dimensions: '30 × 40 cm',
+    title: '"(Inhoud tussen haakjes)"',
+    medium: 'houtskool, verf van uischillen en draad op canvas',
+    dimensions: '17 x 27 x 6,5 m',
     description: '',
     ratio: '3/4',
-    images: [''],
-    thumbnail: '',
+    images: ['images/2026_de fabriek_a.jpg','images/2026_de fabriek_b.jpg','images/2026_de fabriek_c.jpg','images/2026_de fabriek_d.jpg','images/2026_de fabriek_e.jpg',images/2026_de fabriek_f.jpg'],
+    thumbnail: 'images/2026_de fabriek_a.jpg',
   },
   {
     id: 'w2602',
